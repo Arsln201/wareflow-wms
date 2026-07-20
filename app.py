c9fb191ba2085016d6163a0655d0c162f520a5d9
Mirza Arsalan <mirzaarsalan906@gmail.com>
2026-07-20 15:27:16 +0530

    Added warehouse search and QR code generation

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import qrcode
+import os
+
+from flask import Flask, render_template, request, redirect
 from models import db
 from config import Config
+from warehouse_data import WAREHOUSE
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +18,59 @@ with app.app_context():
 def login():
     return render_template("login.html")
 
+@app.route("/login", methods=["POST"])
+def process_login():
+
+    employee_id = request.form.get("employee_id")
+    password = request.form.get("password")
+
+    # Temporary login
+    if employee_id == "EMP1001" and password == "admin123":
+        return redirect("/dashboard")
+
+    return "Invalid Employee ID or Password"
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+
+    location = request.form.get("location").upper()
+
+    if location in WAREHOUSE:
+
+        item = WAREHOUSE[location]
+
+        # Generate QR Code
+        filename = f"{location}.png"
+        filepath = os.path.join("static", "qr", filename)
+
+        img = qrcode.make(location)
+        img.save(filepath)
+
+        return render_template(
+            "qr.html",
+            qr_image=f"/static/qr/{filename}",
+            location=location
+        )
+
+    return """
+    <h1>❌ Location Not Found</h1>
+
+    <br>
+
+    <a href="/search">
+        <button>Try Again</button>
+    </a>
+    """
+
 if __name__ == "__main__":
     print("Starting Flask...")
     app.run(debug=False, use_reloader=False)
+    
---
+++ warehouse_data.py
@@ -0,0 +1,25 @@
+WAREHOUSE = {
+    "A12-04-B": {
+        "product": "Maggi Noodles",
+        "sku": "890105800001",
+        "rack": "A12",
+        "shelf": "04",
+        "bin": "B"
+    },
+
+    "B08-02-A": {
+        "product": "Coca Cola 750ml",
+        "sku": "890176403001",
+        "rack": "B08",
+        "shelf": "02",
+        "bin": "A"
+    },
+
+    "C15-01-D": {
+        "product": "Surf Excel",
+        "sku": "890103086545",
+        "rack": "C15",
+        "shelf": "01",
+        "bin": "D"
+    }
+}
